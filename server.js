61b09189f27579ceaac151a987f0221e5024febc
Wodi <145711630+DarioFot@users.noreply.github.com>
2025-10-17 09:46:05 +0200

    try connection 4

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(express.static(path.join(__dirname, "public")));
 // Serve Stefan assets (HTML is not served here; only static files for sketch usage)
 app.use("/stefan", express.static(path.join(__dirname, "stefan")));
 
-app.get("/", (req, res) => res.redirect("/monitor1"));
-app.get("/monitor1", (req, res) =>
-  res.sendFile(path.join(__dirname, "Petra", "index_petra.html"))
+app.get("/", (req, res) => res.redirect("/index_petra"));
+app.get("/index_petra", (req, res) =>
+  res.sendFile(path.join(__dirname, "public", "index_petra.html"))
 );
 app.get("/monitor2", (req, res) =>
   res.sendFile(path.join(__dirname, "public", "visual.html"))
